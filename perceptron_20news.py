508ed0c91e2d06ad7e7df54e7d62ef6f9717b0bc
Yao Yuguang <yaoyuguang_i@didichuxing.com>
2020-01-17 00:01:38 -0500

    Tony:1. onehot 2. max_iter 3. PCA

--- perceptron_20news.py
+++ perceptron_20news.py
@@ -6,29 +6,40 @@ import numpy as np
 
 class perceptron():
     def __init__(self, dim):
-        self.learning_rate = 0.01
+        self.learning_rate = 10
         self.max_iteration = 200
         self.dim = dim
-        self.w = np.zeros([20, dim + 1])
+        self.w = np.random.random([20, dim + 1])
 
     def train(self, feature, labels):
+        for k in range(self.max_iteration):
+            if (k == 50):
+                self.learning_rate = 1
+            if (k == 100):
+                self.learning_rate = 0.1
 
-        for i in range(np.size(feature, 0)):
-            vector = feature[i]
-            vector = np.insert(vector, self.dim, 1)
-            label = labels[i]
-            # print(self.w.shape)
+            for i in range(np.size(feature, 0)):
+                vector = feature[i]
+                vector = np.insert(vector, self.dim, 1)
+                label = labels[i]
+                # print(label)
+            
+                # print(vector.shape)
+                # print(self.w.shape)
 
-            pred = self.w @ vector
-            pred = self.sigmoid(pred)
-            pred = pred.reshape(20,1)
-            print(pred)
-            l = self.loss(self.onehot(label), pred)
-            # print(l)
-            # print(self.w)
-            self.w = self.w + self.learning_rate * np.multiply( (label - pred), np.multiply(pred, (1 - pred)) ) * vector.reshape(1, self.dim + 1)
-            a = input()
-
+                pred = self.w @ vector
+                # print(pred)
+                pred = self.sigmoid(pred)
+                # print(pred)
+                pred = pred.reshape(20,1)
+                # print(pred)
+                label_onehot = self.onehot(label)
+                l = self.loss(label_onehot, pred)
+                # print(l)
+                # print(self.w)
+                self.w = self.w + self.learning_rate * np.multiply( (label_onehot - pred), np.multiply(pred, (1 - pred)) ) @  vector.reshape(1, self.dim + 1)
+                # a = input() 
+            print(l)
     def onehot(self, label):
         one_hot = np.zeros(20)
         one_hot[label] = 1
@@ -40,27 +51,34 @@ class perceptron():
             vector = features[i]
             vector = np.insert(vector, self.dim, 1)
             pred = self.w @ vector
-            print(pred) 
+            # print(pred) 
             
             pred = self.sigmoid(pred)
             labels.append(np.argmax(pred))
         return labels
 
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        return 1.0 / (1.0 + np.exp(-x))
 
     def loss(self, label, pred):
         return np.mean(np.square(label - pred))
+
+
 
 newsgroups_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42,remove = ('headers', 'footers', 'quotes'))
 newsgroups_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42,remove = ('headers', 'footers', 'quotes'))
 
 vectorizer = TfidfVectorizer(stop_words='english',lowercase=True)
-train_vector = vectorizer.fit_transform(newsgroups_train.data)
+vectorizer.fit(newsgroups_train.data)
+train_vector = vectorizer.transform(newsgroups_train.data)
 test_vector = vectorizer.transform(newsgroups_test.data)
 
-train_pca = decomposition.TruncatedSVD(n_components = 300).fit_transform(train_vector)
-test_pca = decomposition.TruncatedSVD(n_components = 300).fit_transform(test_vector)
+decomp =  decomposition.TruncatedSVD(n_components = 300)
+train_pca = decomp.fit_transform(train_vector)
+test_pca = decomp.transform(test_vector)
+
+
+
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
@@ -71,6 +89,5 @@ per = perceptron(dim = 300)
 per.train(train_pca, newsgroups_train.target)
 pred = per.predict(test_pca)
 
-print(pred)
-print(metrics.f1_score(newsgroups_test.target, pred, average='macro'))
-
+# print(pred)
+print(metrics.accuracy_score(newsgroups_test.target,pred))
