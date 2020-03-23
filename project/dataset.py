a76f46c667b5a0a04f019e9382b786a1d854bb36
merriamt <54546723+merriamt@users.noreply.github.com>
2020-03-23 13:37:52 -0400

    Added BiDirectional LSTM
    I added a functions setting and getting word_to_index, index_to_word, tag_to_index, and index_to_tag dictionaries.

--- project/dataset.py
+++ project/dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class Dataset:
 
-    def __init__(self, name):
+    def __init__(self, name, max_len = 80):
         self.name = name
         self.df_train = None
         self.df_test = None
@@ -11,12 +11,22 @@ class Dataset:
         self.group_test = None
         self.sentences_train = None
         self.sentences_test = None
+        self.prepare_sentences()
+        self.word_to_index = None
+        self.index_to_word = None
+        self.tag_to_index = None
+        self.index_to_tag = None
+        self.prepare_dictionaries()
+        
 
     def get_data(self):
-        train = '../ner/%strain.bio' % self.name
-        test = '../ner/%stest.bio' % self.name
+        train = 'ner/%strain.bio' % self.name
+        test = 'ner/%stest.bio' % self.name
+#         train = '../ner/%strain.bio' % self.name
+#         test = '../ner/%stest.bio' % self.name
         self.df_train = self.read_data(train)
         self.df_test = self.read_data(test)
+                
 
     def read_data(self, filename):
         data_dict = {'sentence_id': [], 'word': [], 'tag': []}
@@ -30,18 +40,40 @@ class Dataset:
                 line = line.strip().split()
                 data_dict['sentence_id'].append(sentence_id)
                 data_dict['word'].append(line[1])
-                data_dict['tag'].append(line[0])
-
+                data_dict['tag'].append(line[0])                
         return pd.DataFrame.from_dict(data_dict)
 
-    def get_sentences(self):
-
+    
+    def prepare_sentences(self):
         self.group_train = self.df_train.groupby('sentence_id', sort=False).apply(lambda s: [(w, t) for w, t in zip(s['word'].values.tolist(),s['tag'].values.tolist())])
         self.group_test = self.df_test.groupby('sentence_id', sort=False).apply(lambda s: [(w, t) for w, t in zip(s['word'].values.tolist(),s['tag'].values.tolist())])
         self.sentences_train = [s for s in self.group_train]
         self.sentences_test = [s for s in self.group_test]
+        
+        
+    def prepare_dictionaries(self):
+        df = pd.concat([self.df_train, self.df_test])
+        words = df['word'].unique()
+        tags = df['tag'].unique()
+        self.word_to_index = {'PADword': 0, 'UNKNOWNword': 1}
+        self.index_to_word = {0: 'PADword', 1: 'UNKNOWNword'}
+        for i, word in enumerate(words):
+            self.word_to_index[word] = i+2
+            self.index_to_word[i+2] = word
+        self.tag_to_index = {'PADword': 0}
+        self.index_to_tag = {0: 'PADword'}
+        for i, tag in enumerate(tags):
+            self.tag_to_index[tag] = i+1
+            self.index_to_tag[i+1] = tag
+    
+    def get_sentences(self):
         return self.sentences_train, self.sentences_test
+    
 
+    def get_dictionaries(self):
+        return self.word_to_index, self.index_to_word, self.tag_to_index, self.index_to_tag
+     
+    
     def save(self):
         '''save processed dataframe'''
         raise NotImplementedError
